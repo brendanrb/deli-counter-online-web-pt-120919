85d8fcf0e7122c3ec5527789a014c1046423a2f0
Brendan R Borgers <brendanborgers@gmail.com>
2019-12-16 17:45:15 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,14 +1,13 @@
 katz_deli = []
 
 
-def line (array)
-  if katz_deli.size == 0
-    puts "The line is currently empty."
-  
-  else
-    puts "The line is currently:"
+def line_simple(array) 
+  current_line = "The line is currently:"
+  array.each.with_index(1) do |name, index_place|  
+    current_line << " #{index_place}. #{name},"    
   end
-end
+  puts current_line
+end 
 
 def take_a_number(line, new_person)
   line << new_person
