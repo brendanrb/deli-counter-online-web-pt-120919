e8ac89e47c2e275a5642ef351928b7454a2cd7cb
Brendan R Borgers <brendanborgers@gmail.com>
2019-12-16 17:30:08 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -10,9 +10,10 @@ def line (katz_deli)
   end
 end
 
-def take_a_number(katz_deli, name){
+function takeANumber(katz_deli, name) {
   katz_deli.push(name)
-  return "Welcome, " + name + ". You are number " + katz_deli.length + " in line."}
+  return "Welcome, " + name + ". You are number " + katz_deli.length + " in line."
+}
 
 
 end  
