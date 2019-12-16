98208eeea4fe2a450c61031d4591bd7d939adfe2
Brendan R Borgers <brendanborgers@gmail.com>
2019-12-16 17:41:12 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -9,3 +9,8 @@ def line (katz_deli)
     puts "The line is currently:"
   end
 end
+
+def take_a_number(line, new_person)
+  line.push(new_person) # could say: "line << new_person"
+  puts "Welcome, #{new_person}. You are number #{line.length} in line."
+end
