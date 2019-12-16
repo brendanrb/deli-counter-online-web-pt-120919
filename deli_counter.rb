85bae03d4b70d08658fbbbc331e7a334cf2c4709
Brendan R Borgers <brendanborgers@gmail.com>
2019-12-16 17:10:21 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,1 +1,16 @@
-# Write your code here.
+
+def line (katz_deli)
+  if katz_deli.size == 0
+    puts "The line is currently empty."
+  
+  else
+    puts "The line is currently:"
+  end
+end
+
+def take_a_number(katz_deli, name)
+  
+puts "Welcome, #{name}. You are number #{name.index} in line"
+
+  
+end  
